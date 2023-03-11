28d2acce1df2cf34cfe4f7852d4ad83fa91f0efa
Renan Monteiro <monteiro-renan@hotmail.com>
2023-03-11 17:57:47 -0300

    configure validation

--- src/main/java/com/renan/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/com/renan/springboot2/requests/AnimePostRequestBody.java
@@ -2,7 +2,10 @@ package com.renan.springboot2.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty(message = "Nome não pode ser vazio.")
     private String name;
 }
