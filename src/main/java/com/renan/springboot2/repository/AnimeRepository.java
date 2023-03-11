8a5c104b2dc34f2c38c5690e82d1e40c273a5692
Renan Monteiro <monteiro-renan@hotmail.com>
2023-03-11 10:23:55 -0300

    create findByName

--- src/main/java/com/renan/springboot2/repository/AnimeRepository.java
+++ src/main/java/com/renan/springboot2/repository/AnimeRepository.java
@@ -3,6 +3,8 @@ package com.renan.springboot2.repository;
 import com.renan.springboot2.domain.Anime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnimeRepository extends JpaRepository<Anime, Long> {
+import java.util.List;
 
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+    List<Anime> findByName(String name);
 }
