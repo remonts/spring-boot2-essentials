a84f2f3f7894d68f769d56fc084beb914772b915
Renan Monteiro <monteiro-renan@hotmail.com>
2023-03-11 16:17:54 -0300

    configure transactional

--- src/main/java/com/renan/springboot2/service/AnimeService.java
+++ src/main/java/com/renan/springboot2/service/AnimeService.java
@@ -9,6 +9,7 @@ import com.renan.springboot2.requests.AnimePutRequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -29,6 +30,7 @@ public class AnimeService {
                 .orElseThrow(() -> new BadRequestException("Anime not found"));
     }
 
+    @Transactional
     public Anime save(AnimePostRequestBody animePostRequestBody) {
         return animeRepository.save(AnimeMapper.INSTANCE.toAnime(animePostRequestBody));
     }
